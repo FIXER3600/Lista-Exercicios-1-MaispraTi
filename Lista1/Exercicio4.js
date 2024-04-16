c363fb108670240c3daecebe8d2250fb76c18a24
FIXER3600 <guilhermecomercial51@gmail.com>
2024-04-16 19:48:51 -0300

    Exercicio 4 e 5 feitos

--- Lista1/Exercicio4.js
+++ Lista1/Exercicio4.js
@@ -1,3 +1,5 @@
 const prompt = require("prompt-sync")();
-let nota1=Number(prompt("Digite a primeira nota:"));
-let nota2=Number(prompt("Digite a segunda nota:"))
+let nota1=Number(prompt("Digite a nota da primeira avaliação:"));
+let nota2=Number(prompt("Digite a nota da segunda avaliação:"))
+const media=(nota1+nota2)/2
+console.log(`${media>=6? 'PARABÉNS! Você foi aprovado':'Você foi REPROVADO! Estude mais'}`);
