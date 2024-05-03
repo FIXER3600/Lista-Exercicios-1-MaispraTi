804fe6508d460171e47413a9ca8a4f0055217b46
FIXER3600 <guilhermecomercial51@gmail.com>
2024-05-03 02:35:45 -0300

    feat: exercício 4 que verifica se 3 retas formam um triangulo

---
+++ Lista2/Exercicio4.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")();
+let a = Number(prompt("Digite o comprimento da priimeira reta:"));
+let b = Number(prompt("Digite o comprimento da segunda reta:"));
+let c = Number(prompt("Digite o comprimento da terceira reta:"));
+let message=''
+if (a<b+c && b<a+c && c<a+b) {
+	message='É possível formar um triângulo pelos comprimentos fornecidos.'
+}else{
+	message='Pelos lados fornecidos, não se forma um triângulo válido'
+}
+console.log(message);
