958fbe1d46f877b64357042c44e5e1528b72496b
FIXER3600 <guilhermecomercial51@gmail.com>
2024-05-04 23:34:44 -0300

    feat: exercício 14 Listando inversamente 7 pessoas cadastradas

---
+++ Lista2/Exercicio14.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+const people=[]
+let person=''
+for (let i = 0; i < 8; i++) {
+	person=prompt(`Digite a ${i}º pessoa: `)
+	people.push(person)
+	
+}
+console.log(people.reverse());
